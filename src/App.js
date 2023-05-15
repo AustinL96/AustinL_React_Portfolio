d3f64f588a48b2aca7e07c22bf55248f228c9657
Austin Lewis <austinlewis165@gmail.com>
2023-05-14 23:16:57 -0400

    refactored to fix build issues

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import { useState } from "react";
-
-import Header from './Components/UI/Header.js';
-import AboutMe from './Components/Pages/AboutMe.js';
-import Project from './Components/Pages/Project.js';
-import Contact from './Components/Pages/Contact.js';
-import Footer from './Components/UI/Footer.js';
+import Header from "./Components/UI/Header";
+import AboutMe from "./Components/Pages/AboutMe";
+import Project from "./Components/Pages/Project";
+import Contact from "./Components/Pages/Contact";
+import Footer from "./Components/UI/Footer";
 
 function App() {
-  const [activeLink, setActiveLink] = useState('AboutMe');
+  const [activeLink, setActiveLink] = useState("AboutMe");
 
   const handleSetActiveLink = (link) => {
     setActiveLink(link);
   };
 
+  let activePage;
+  switch (activeLink) {
+    case "AboutMe":
+      activePage = <AboutMe />;
+      break;
+    case "Project":
+      activePage = <Project />;
+      break;
+    case "Contact":
+      activePage = <Contact />;
+      break;
+    default:
+      activePage = null;
+  }
+
   return (
     <>
       <Header setActiveLink={handleSetActiveLink} activeLink={activeLink} />
-      {activeLink === "AboutMe" && <AboutMe />}
-      {activeLink === "Project" && <Project />}
-      {activeLink === "Contact" && <Contact />}
+      {activePage}
       <Footer />
     </>
   );
--- src/Components/UI/Navigation.js
+++ src/Components/UI/Navigation.js
@@ -1,36 +1,42 @@
 
 
 function Navigation(props) {
+  const handleLinkClick = (link, event) => {
+    event.preventDefault();
+    props.setActiveLink(link);
+  };
 
-    return (
-        <nav>
-          <a 
-            href="#" 
-            className={props.activeLink === "AboutMe" ? "active" : ""}
-            onClick={() => props.setActiveLink("AboutMe")}
-          >
-            About Me
-          </a>
-          <span>|</span>
-          <a 
-            href="#" 
-            className={props.activeLink === "Project" ? "active" : ""}
-            onClick={() => props.setActiveLink("Project")}
-          >
-            Portfolio
-          </a>
-          <span>|</span>
-          <a 
-            href="#" 
-            className={props.activeLink === "Contact" ? "active" : ""}
-            onClick={() => props.setActiveLink("Contact")}
-          >
-            Contact
-          </a>
-          <span>|</span>
-          <a href="/AustinLDeveloperResume.pdf" download>Download My Resume 📩</a>
-        </nav>
-      );
+  return (
+    <nav>
+      <a
+        href="/aboutme"
+        className={props.activeLink === "AboutMe" ? "active" : ""}
+        onClick={(e) => handleLinkClick("AboutMe", e)}
+      >
+        About Me
+      </a>
+      <span>|</span>
+      <a
+        href="/portfolio"
+        className={props.activeLink === "Project" ? "active" : ""}
+        onClick={(e) => handleLinkClick("Project", e)}
+      >
+        Portfolio
+      </a>
+      <span>|</span>
+      <a
+        href="/contact"
+        className={props.activeLink === "Contact" ? "active" : ""}
+        onClick={(e) => handleLinkClick("Contact", e)}
+      >
+        Contact
+      </a>
+      <span>|</span>
+      <a href="/AustinLDeveloperResume.pdf" download>
+        Download My Resume 📩
+      </a>
+    </nav>
+  );
 }
 
-export default Navigation
+export default Navigation;
